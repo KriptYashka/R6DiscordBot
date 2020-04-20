1c5550139dfbe187e2b21e7d1340b3e71682bd1b
Егор Кучук <58609689+KriptYashka@users.noreply.github.com>
2020-04-20 15:48:48 +0300

    Create base bot - "Jager"
    Бот имеет функционал:
    - привет ( здоровается с пользователем )
    - рейт {Nick} ( показывает рейтинг пользоваеля )
    - рейт_вместе {*Nicks} ( показывает, можно ли играть вместе в рейтинг )
    - дай {роль|статистику Nick|мою} ( показывает статистику )
    - запомни {Nick} ( запоминает Nick пользователя, присылает данные в канал, где id = channel_memory_id)
    - как играть в ... ( объясняет игроку, как научиться играть в игру )
    - удали {n} ( удаляет n сообщений в текущем канале }
    - меню_группировок ( создает сообщение с реакциями, которые дают роли )
    - инструкция ( создает инструкцию к боту )

---
+++ jager.py
@@ -0,0 +1,228 @@
+import discord
+from discord.ext import commands
+import random
+import r6sapi as api
+
+bot = commands.Bot(command_prefix='Ягер ')
+TOKEN = 'your_token'
+email = "your_email"
+password = "your_password"
+
+EU = api.RankedRegions.EU
+channel_memory_id = 701698041660309574 # id канала для сохранения R6-ников пользователей
+
+text_np = ['Ну давай попробуем...', 'Ладно, щя сделаем.', 'Без проблем.',
+           'Чуть синей изоленты и... Готово!', 'За **ACOG** сделаю всё, что угодно.',
+           'Потерпи чуть, я не такой быстрый. У меня 2 скорости.\n*звуки плача*',
+           'Ставлю **СОГ**.', 'Vierundzwanzigstundenglück!']
+text_hello = ['Guten Tag, {}', 'Здаров {}', 'Привет {}', '{} и тебе привет.']
+# id эмодзи и название ролей
+emoji_roles = {701007355956953148: 'Ассасин', 701007347904151605: 'Наёмник',
+               701007349338603570: 'Док', 701007348730167346: 'Военный', 701007347534921750: 'Анархист'}
+
+
+def get_random_item(array):
+    index_arr = random.randint(0, len(array) - 1)
+    return array[index_arr]
+
+
+async def clear_channel(channel, n=1000):
+    async for message in channel.history(limit=n):
+        await message.delete()
+
+
+async def find_nick_in_memory(user_id):
+    channel_memory = bot.get_channel(channel_memory_id)
+    async for message in channel_memory.history():
+        if message.content.startswith(str(user_id)):
+            return message.content.split()[1]
+
+
+async def give_role(ctx, role_name):
+    member = ctx.message.author
+    server = ctx.message.guild
+    role = discord.utils.get(server.roles, name=role_name)
+    if role is None:
+        return await ctx.send('Нет такой роли. Не трать моё время!')
+    if role_name == 'Бармен':
+        return await ctx.send('Нее, браток. У нас бармен единственный - **KriptYashka**.')
+    await member.add_roles(role)
+    await ctx.send(get_random_item(text_np))
+
+
+async def send_statistic_r6(ctx, nick):
+    # text_statistic = "**Статистика игрока {}** {}\n\n".format(nick, str(bot.get_emoji(700596539499872256)))
+    await ctx.send(get_random_item(text_np))
+    auth = api.Auth(email, password)
+    player = await auth.get_player(nick, api.Platforms.UPLAY)
+    await player.load_general()
+    rank = await player.get_rank(EU)
+    mmr = int(rank.mmr)
+
+    hours = int(player.time_played / 60 / 60)
+    if 2 <= hours % 10 <= 4:
+        str_hours = str(hours) + " часа"
+    elif hours % 10 == 1:
+        str_hours = str(hours) + " час"
+    else:
+        str_hours = str(hours) + " часов"
+
+    embed = discord.Embed(title="Статистика " + nick, color=0x7d17bb)
+    embed.set_author(name="Rainbow Six: Siege", icon_url=player.icon_url)
+    embed.set_thumbnail(
+        url=rank.get_icon_url())
+    embed.add_field(name="Текущее звание:", value=rank.get_bracket_name(), inline=True)
+    embed.add_field(name="Рейтинг:", value=str(mmr), inline=False)
+    embed.add_field(name="Убийства: ", value=player.kills, inline=True)
+    embed.add_field(name="Смерти", value=player.deaths, inline=True)
+    embed.add_field(name="Убийства/Смерти:", value="{:.2f}"
+                    .format(player.kills/player.deaths), inline=True)
+    embed.add_field(name="Победы:", value=player.matches_won, inline=True)
+    embed.add_field(name="Поражения:", value=player.matches_lost, inline=True)
+    embed.add_field(name="Победы/Поражения:", value="{:.2f}"
+                    .format(player.matches_won/player.matches_lost), inline=True)
+    embed.add_field(name="Время игры:", value=str_hours, inline=False)
+
+    await ctx.send(embed=embed)
+    auth.close()
+
+
+@bot.event
+async def on_ready():
+    print('{0.user} пришел на сервера'.format(bot))
+
+
+@bot.event
+async def on_member_join(member):
+    server = member.guild
+    role = discord.utils.get(server.roles, name='Новичок')
+    await member.add_roles(role)
+
+
+@bot.event
+async def on_raw_reaction_add(event):
+    emoji = event.emoji
+    user = event.member
+    if bot.user == user:
+        return
+    if emoji_roles[emoji.id] is not None:
+        role = discord.utils.get(user.guild.roles, name=emoji_roles[emoji.id])
+        await user.add_roles(role)
+
+
+@bot.event
+async def on_raw_reaction_remove(event):
+    emoji = event.emoji
+    guild = bot.get_guild(event.guild_id)
+    member = guild.get_member(event.user_id)
+
+    if bot.user == member:
+        return
+    print(emoji.id)
+    if emoji_roles[emoji.id] is not None:
+        role = discord.utils.get(member.guild.roles, name=emoji_roles[emoji.id])
+        await member.remove_roles(role)
+
+
+@bot.command(pass_context=True)
+async def привет(ctx):
+    text = get_random_item(text_hello).format(str(ctx.message.author.name))
+    await ctx.send(text)
+
+
+@bot.command(pass_context=True)
+async def как(ctx, args):
+    if args == 'научиться' or args == 'играть':
+        text = "Никак. Удали к чертям эту игру!"
+        await ctx.send(text)
+
+
+@bot.command(pass_context=True)
+async def удали(ctx, arg):
+    try:
+        await ctx.send(get_random_item(text_np))
+        if arg == 'все' or arg == 'всё' or arg is None:
+            await clear_channel(ctx.channel)
+        else:
+            n = int(arg)
+            await clear_channel(ctx.channel, n+2)
+    except TypeError:
+        await ctx.send("Не совсем понял тебя.")
+
+
+@bot.command(pass_context=True)
+async def меню_группировок(ctx):
+    channel_role = bot.get_channel(700806731260755978)
+    await clear_channel(channel_role)
+    text = "Пока сервер развивается, я мало что могу сделать для тебя.\nХоть ты и так **охотник**, " \
+           "наверное, но я могу отнести тебя к какой хочешь групировке..."
+    for item in emoji_roles.items():
+        text += "\n - " + str(bot.get_emoji(item[0])) + " " + item[1]
+    text += "\nВыбирай одну роль."
+
+    message = await channel_role.send(text)
+    for id_emoji in emoji_roles:
+        await message.add_reaction(bot.get_emoji(id_emoji))
+
+
+@bot.command(pass_context=True)
+async def дай(ctx, command, *args):
+    if command == "роль":
+        await give_role(ctx, args[0])
+    if command == "статистику":
+        if args[0] == "мою":
+            nick = await find_nick_in_memory(ctx.author.id)
+            if nick is None:
+                await ctx.send('Я не знаю твоего ника в R6.')
+            else:
+                await send_statistic_r6(ctx, nick)
+
+
+@bot.command(pass_context=True)
+async def рейт(ctx, nick):
+    await ctx.send(get_random_item(text_np))
+    auth = api.Auth(email, password)
+    player = await auth.get_player(nick, api.Platforms.UPLAY)
+    rank = await player.get_rank(EU)
+    await ctx.send("**" + nick + "** - " + str(int(rank.mmr)) + ' MMR')
+    await auth.close()
+
+
+@bot.command(pass_context=True)
+async def рейт_вместе(ctx, *nicks):
+    await ctx.send(get_random_item(text_np))
+    auth = api.Auth(email, password)
+    max_rank = 0
+    min_rank = 10000
+    for nick in nicks:
+        player = await auth.get_player(nick, api.Platforms.UPLAY)
+        rank = await player.get_rank(EU)
+        max_rank = max(max_rank, int(rank.mmr))
+        min_rank = min(min_rank, int(rank.mmr))
+    delta = max_rank - min_rank
+    if delta >= 1000:
+        text = "К сожалению, вы не сможете сыграть... \nТекущая разница: " + str(delta) + " MMR"
+    else:
+        text = "Можно играть без проблем! \nТекущая разница: " + str(delta) + " MMR"
+    await ctx.send(text)
+    await auth.close()
+
+
+@bot.command(pass_context=True)
+async def запомни(ctx, command, *args):
+    channel_memory = bot.get_channel(channel_memory_id)
+    if command == "меня":
+        user_id = ctx.author.id
+        await channel_memory.send(str(user_id) + " " + args[0])
+        await ctx.send(get_random_item(text_np))
+
+
+@bot.command(pass_context=True)
+async def инструкция(ctx):
+    await clear_channel(ctx.channel, 1)
+    guild_name = ctx.guild.name
+    emoji = str(bot.get_emoji(701757818965065759)) # Pulse, id_emoji
+    text = "{} Привет, ты на сервере **{}** {}".format(emoji, guild_name, emoji)
+    await ctx.send(text)
+
+bot.run(TOKEN)
