adfc9a8095c3f31785e2ba8256bcecd1165f6477
Егор Кучук <kriptyashka@gmail.com>
2022-01-26 15:43:59 +0300

    Update data.py

--- view/data.py
+++ view/data.py
@@ -1,14 +1,5 @@
-import discord
-import sqlite3
 from discord.ext import commands
 from classes.Database import DataBaseR6
-
-channel_memory_id = 701698041660309574
-guild_id = 700357287700594708
-
-
-def get_channel_memory(bot):
-    return bot.get_channel(channel_memory_id)
 
 
 def get_nickname(bot: commands.Bot, discord_id: int):
@@ -20,16 +11,8 @@ def get_nickname(bot: commands.Bot, discord_id: int):
 
 
 async def get_all_memory(bot):
-    list_messages = []
-    channel_memory = get_channel_memory(bot)
-    async for message in channel_memory.history():
-        list_messages.append(message.content)
-    return list_messages
+    pass
 
 
 async def get_nicks_memory(bot):
-    list_players = []
-    channel_memory = get_channel_memory(bot)
-    async for message in channel_memory.history():
-        list_players.append(message.content.split()[1])
-    return list_players
+    pass
